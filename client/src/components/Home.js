80d1f5cf322d9ae2a4485f3d67bbf6490046a2e7
Joseph Ferro <joejoebeef@gmail.com>
2019-01-05 12:16:18 -0700

    add video grid

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,56 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Segment, Grid } from 'semantic-ui-react';
+import Iframe from 'react-iframe'
 
 class Home extends React.Component {
   render() {
     return (
-      <Header as = 'h2' textAlign = 'center'>UToob</Header>
+      <Segment basic>        
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
+                width="470px"
+                height="250px"
+                display="initial"
+                position="relative"
+                styles={{borderRadius: "50px"}}
+                allowFullScreen
+              />
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
+                width="470px"
+                height="250px"
+                display="initial"
+                position="relative"
+                allowFullScreen
+              />
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
+                width="470px"
+                height="250px"
+                display="initial"
+                position="relative"
+                allowFullScreen
+              />
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
+                width="470px"
+                height="250px"
+                display="initial"
+                position="relative"
+                allowFullScreen
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
     )
   }
 }
