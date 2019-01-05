012a8674e3b7ac95e3df2ca068c0c7c98c8ad3e4
Joseph Ferro <joejoebeef@gmail.com>
2019-01-05 11:44:58 -0700

    add init middleware and devise-axios

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,10 @@ import App from './App';
 import { BrowserRouter, } from "react-router-dom";
 import { AuthProvider, } from "./providers/AuthProvider";
 import * as serviceWorker from './serviceWorker';
+import { initMiddleware, } from "devise-axios";
 import 'semantic-ui-css/semantic.min.css';
+
+initMiddleware();
 
 ReactDOM.render(
   <AuthProvider>
