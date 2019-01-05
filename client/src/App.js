f29e931e30e959d3aa8ffcad59aa43e905399f69
Joseph Ferro <joejoebeef@gmail.com>
2019-01-05 10:20:13 -0700

    add basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 import NavBar from './components/NavBar';
 import NoMatch from './components/NoMatch';
-
+  
 class App extends Component {
   render() {
     return (
