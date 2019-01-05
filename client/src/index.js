9303a7f2bef1d9f3920070fa98e8ea820a509c2b
Joseph Ferro <joejoebeef@gmail.com>
2019-01-05 10:17:14 -0700

    add basic routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { BrowserRouter, } from "react-router-dom";
+import { AuthProvider, } from "./providers/AuthProvider";
 import * as serviceWorker from './serviceWorker';
+import { initMiddleware, } from "devise-axios";
+import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+initMiddleware();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(
+  <AuthProvider>
+    <BrowserRouter>
+      <App /> 
+    </BrowserRouter>
+  </AuthProvider>,
+  document.getElementById('root')
+);
+
 serviceWorker.unregister();
