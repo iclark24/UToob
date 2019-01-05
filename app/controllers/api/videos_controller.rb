17aa7a654f51ebd3447ab304489fda01497ece83
Jessica Anderson <jessie.leigh30@gmail.com>
2019-01-05 10:26:19 -0700

    video model and controller

---
+++ app/controllers/api/videos_controller.rb
@@ -0,0 +1,43 @@
+class Api::VideosController < ApplicationController
+  before_action :set_video, only: [:show, :update, :destroy]
+  
+  def index
+    render json: Video.all
+  end
+
+  def show
+    render json: @video
+  end
+
+  def create
+    video = Video.new(video_params)
+    if video.save
+      render json: video
+    else
+      render json: video.errors
+    end
+ end
+
+  def update
+    if @video.update(video_params)
+      render json: @video
+    else 
+      render json: @video.errors
+  end
+end
+
+  def destroy
+    @video.destroy
+  end
+
+  private
+
+  def set_video
+    @video = Video.find(params[:id])
+  end
+
+  def video_params
+    params.require(:video).permit(:video)
+  end
+
+end
